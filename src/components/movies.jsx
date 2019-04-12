7704f14e265744cbab35a87d22dc2506876c5e09
halalan <halalanrizkon52@gmail.com>
2019-04-12 16:56:38 +0700

    no fix 24- Replacing FakeGenreService

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,7 +5,7 @@ import Pagination from "./common/pagination";
 import { getMovies, deleteMovie  } from "../services/fakeMovieService";
 import { getGenres } from "../services/genreService";
 import { paginate } from "../utils/paginate";
-import {Link} from "react-router-dom";
+import {Link} from "react-router-dom"
 import _ from "lodash";
 import SearchBox from "./searchBox";
 
