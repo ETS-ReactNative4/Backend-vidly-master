9048f7ee204db08e3ec82e5d9af83b2033b51f46
halalan <halalanrizkon52@gmail.com>
2019-04-18 14:33:17 +0700

    30- Saving the Movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,7 +5,20 @@ const apiEndpoint = apiUrl + "/movies";
 export function getMovies() {
   return http.get(apiEndpoint);
 }
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + "/" + movieId);
+}
+export function saveMovie(movie) {
+ if (movie._id) {
+   const body = {...movie};
+   delete body._id;
+  return http.put(apiEndpoint + '/' + movie._id, body)
+ }
+
+return http.post(apiEndpoint, movie);
+
+}
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
-}
+ return http.delete(apiEndpoint + "/" + movieId);
+} 
